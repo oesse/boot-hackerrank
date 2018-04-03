e34a21c3f16ce0ee7d83e66bc636f4b00aa76d8f
Oliver Esser <oliver.esser@campus.tu-berlin.de>
2018-04-03 12:53:14 +0200

    fix: pipe input into executable in io test runner

--- tools/test-runner.sh
+++ tools/test-runner.sh
@@ -4,4 +4,4 @@ project_root=$tools_dir/..
 readio="$tools_dir/read-io.sh"
 exe=$1
 iofile=$project_root/$2
-diff <($exe <($readio -i $iofile)) <($readio -o $iofile)
+diff <($readio -i $iofile | $exe) <($readio -o $iofile)
